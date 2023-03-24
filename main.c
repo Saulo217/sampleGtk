8fb696191a855bd6e4dff769d506e40a8a28a4ff
saulo <saulo@saulo.com>
2023-03-24 15:00:02 -0300

    modifications

--- main.c
+++ main.c
@@ -4,6 +4,10 @@ static void print_hello(GtkWidget *widget, gpointer data) {
 	g_print("Hello World!\n");
 }
 
+static void sec_hello(GtkWidget *widget, gpointer) {
+	g_print("Second Hello World!\n");
+}
+
 int main(int argc, char *argv[]) {
 	GtkBuilder *builder;
 	GObject *window;
@@ -22,13 +26,13 @@ int main(int argc, char *argv[]) {
 	window = gtk_builder_get_object(builder, "window");
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-	button = gtk_builder_get_object(builder, "button1");
+	button = gtk_builder_get_object(builder, "first");
 	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
 
-	button = gtk_builder_get_object(builder, "button2");
-	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+	button = gtk_builder_get_object(builder, "second");
+	g_signal_connect(button, "clicked", G_CALLBACK(sec_hello), NULL);
 
-	button = gtk_builder_get_object(builder, "quit");
+	button = gtk_builder_get_object(builder, "exit");
 	g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
 
 	gtk_main();
